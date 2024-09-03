69b14b88081bacdf2f0bfc427605933c555aba65
Eric <eroija@gmail.com>
2024-09-03 11:30:49 +0300

    index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,38 +7,26 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
+    """GET /api/v1/status."""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
+    """GET /api/v1/stats."""
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """GET /api/v1/unauthorized
-    Return:
-      - Unauthorized error.
-    """
+@app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
+def unauthorized_endpoint() -> None:
+    """Endpoint that raises a 401 error."""
     abort(401)
 
 
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """GET /api/v1/forbidden
-    Return:
-      - Forbidden error.
-    """
+@app_views.route('/forbidden/', strict_slashes=False, methods=['GET'])
+def forbidden_endpoint() -> None:
+    """Endpoint that raises a 403 error."""
     abort(403)
